9227928e7df28f8594d314ffb1d7d9bdc2735be2
Preston Edwards <pres@imdiversity.com>
2019-04-18 13:36:22 -0500

    added landmarks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import landmarks from "./landmark.json";
 
 class App extends Component {
+  constructor (){
+    super()
+    this.state=
+    {landmarks:landmarks}
+  }
   render() {
+
+    console.log (this.state.landmarks)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="header">
+          <h1>Clicky Game</h1>
+          <h1>Score</h1>
+          <h1>TOP Score</h1>
+          <h1>You Guess correctly</h1>
+
+        </div>
+        <div className="imagecontainer">
+        {
+          this.state.landmarks.map(landmark => {
+            console.log(landmark.image)
+            return (<img key = {landmark.name} src = {landmark.image } alt={landmark.name}></img>)
+          })
+        }
+         </div>
       </div>
     );
   }
